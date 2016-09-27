d27454333e0ac401ba842355d73a6d26a676f593
Vitalik Maksymlyuk <vitalikmaksymlyuk@Vitaliks-Mac.local>
2016-09-27 14:01:16 +0300

    Initial Commit

---
+++ justLearningiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  justLearningiOS
+//
+//  Created by Vitalik Maksymlyuk on 27.09.16.
+//  Copyright © 2016 Vitalik Maksymlyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
